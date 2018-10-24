b8a1670f0cef6387464fdd800ee9abb4c23ed87c
Darren <donaldwu1101@gmail.com>
2018-10-24 18:01:15 +0800

    inital commit

---
+++ VaporPush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VaporPush
+//
+//  Created by 吳得人 on 2018/10/3.
+//  Copyright © 2018 吳得人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Stop")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
